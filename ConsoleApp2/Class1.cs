0b2e5711f77b2ebbd455357ed5d1e3e19cc3bbf4
Nikola Kovačević <nkovac01@fesb.hr>
2022-03-15 20:00:46 +0100

    Update Class1.cs

--- ConsoleApp2/Class1.cs
+++ ConsoleApp2/Class1.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp2
     //comment
     class Class1
     {
-        static void Main(string[] args)
+        static void Main(char args)
         {
             Console.WriteLine("Novo");
         }
